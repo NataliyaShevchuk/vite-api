c788719ab2550ff02c25388f9892a98db18bcfc3
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-12-13 02:54:21 +0100

    aggiunte le card dinamicamente

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,28 +1,41 @@
 <script>
+import axios from "axios";
+import { store }  from "../store.js";
+
 export default{
     data(){
         return{
-
+            LocalHostError: "",
+            store,
         }
     },
     created(){
         axios.get("https://rickandmortyapi.com/api/character")
         .then( (resp) => {
-            console.log(resp);
+            console.log(resp.data.results);
+
+            this.store.CharacterData = resp.data.results;
         })
+        .catch(() => {
+            this.LocalHostError = "A causa di un guasto l'operazione non è andata a buon fine. Riprova più tardi.";
+        });
     }
 }
 </script>
 
 <template>
-    <div>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col">
+    <div class="container">
+        <div class="alert alert-primary" v-if="LocalHostError">
+            {{ LocalHostError }}
+        </div>
+        <div class="row row-cols-5 g-4 pt-3">
+            <div class="col" 
+            v-for="(character, index) in store.CharacterData" :key="index">
                 <div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
+                <img :src="character.image" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title"> {{ character.name }} </h5>
+                    <p class="card-text"> {{ character.species}} </p>
                 </div>
                 </div>
             </div>
